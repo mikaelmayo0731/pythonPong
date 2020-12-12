5ed66eaf21736f8cd022f5ef9917bbb9b582ca6a
mikael <mikaelmayo0731@yahoo.com>
2020-12-11 16:29:26 -0800

    Added scoring system

--- Pong.py
+++ Pong.py
@@ -4,13 +4,18 @@ import random
 
 
 def ball_animations():
-    global ball_speed_x, ball_speed_y
+    global ball_speed_x, ball_speed_y, opponent_score, player_score
     ball.x += ball_speed_x
     ball.y += ball_speed_y
 
     if ball.top <= 0 or ball.bottom >= screen_height:
         ball_speed_y *= -1
-    if ball.left <= 0 or ball.right >= screen_width:
+
+    if ball.left <= 0:
+        opponent_score += 1
+        ball_restart()
+    if ball.right >= screen_width:
+        player_score += 1
         ball_restart()
 
     if ball.colliderect(player) or ball.colliderect(opponent):
@@ -37,31 +42,42 @@ def opponent_ai():
 
 def ball_restart():
     global ball_speed_x, ball_speed_y
-    ball.center = (screen_width/2, screen_height/2)
+    ball.center = (screen_width / 2, screen_height / 2)
     ball_speed_y *= random.choice((1, -1))
     ball_speed_x *= random.choice((1, -1))
 
 
+# General Setup
 pygame.init()
 clock = pygame.time.Clock()
 
+# Main Window
 screen_width = 600
 screen_height = 500
 screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption("PyPong")
 
-ball = pygame.Rect(screen_width/2 - 10, screen_height/2 - 10, 15, 15)
-opponent = pygame.Rect(screen_width - 20, screen_height/2 - 50, 10, 100)
-player = pygame.Rect(10, screen_height/2 - 50, 10, 100)
+# Game Rectangles
+ball = pygame.Rect(screen_width / 2 - 10, screen_height / 2 - 10, 15, 15)
+opponent = pygame.Rect(screen_width - 20, screen_height / 2 - 50, 10, 100)
+player = pygame.Rect(10, screen_height / 2 - 50, 10, 100)
 
+# Colors
 bg_color = pygame.Color('grey12')
 light_grey = (200, 200, 200)
 
-ball_speed_x = 5
-ball_speed_y = 5
+# Game Variables
+ball_speed_x = 5 * random.choice((1, -1))
+ball_speed_y = 5 * random.choice((1, -1))
 player_speed = 0
 opponent_speed = 7
 
+# Text Variables
+player_score = 0
+opponent_score = 0
+game_font = pygame.font.Font("freesansbold.ttf", 16)
+
+# Game Loop
 run = True
 while run:
     for event in pygame.event.get():
@@ -79,6 +95,7 @@ while run:
             if event.key == pygame.K_DOWN:
                 player_speed -= 7
 
+    # Game Logic
     player_animations()
     opponent_ai()
     ball_animations()
@@ -88,7 +105,12 @@ while run:
     pygame.draw.rect(screen, light_grey, player)
     pygame.draw.rect(screen, light_grey, opponent)
     pygame.draw.ellipse(screen, light_grey, ball)
-    pygame.draw.aaline(screen, light_grey, (screen_width/2, 0), (screen_width/2, screen_height))
+    pygame.draw.aaline(screen, light_grey, (screen_width / 2, 0), (screen_width / 2, screen_height))
+
+    player_text = game_font.render(f"{player_score}", False, light_grey)
+    screen.blit(player_text, (screen_width / 2 - 13, screen_height / 2))
+    opponent_text = game_font.render(f"{opponent_score}", False, light_grey)
+    screen.blit(opponent_text, (screen_width / 2 + 5, screen_height / 2))
 
     pygame.display.flip()
     clock.tick(60)
