5a7289478ebc14530da85fa7b6a5c8f9a43778a0
mikael <mikaelmayo0731@yahoo.com>
2020-12-11 17:10:57 -0800

    Added countdown timer

--- Pong.py
+++ Pong.py
@@ -4,7 +4,7 @@ import random
 
 
 def ball_animations():
-    global ball_speed_x, ball_speed_y, opponent_score, player_score
+    global ball_speed_x, ball_speed_y, opponent_score, player_score, score_time
     ball.x += ball_speed_x
     ball.y += ball_speed_y
 
@@ -13,10 +13,11 @@ def ball_animations():
 
     if ball.left <= 0:
         opponent_score += 1
-        ball_restart()
+        score_time = pygame.time.get_ticks()
+
     if ball.right >= screen_width:
         player_score += 1
-        ball_restart()
+        score_time = pygame.time.get_ticks()
 
     if ball.colliderect(player) or ball.colliderect(opponent):
         ball_speed_x *= -1
@@ -41,10 +42,28 @@ def opponent_ai():
 
 
 def ball_restart():
-    global ball_speed_x, ball_speed_y
+    global ball_speed_x, ball_speed_y, score_time
     ball.center = (screen_width / 2, screen_height / 2)
-    ball_speed_y *= random.choice((1, -1))
-    ball_speed_x *= random.choice((1, -1))
+
+    current_time = pygame.time.get_ticks()
+    if current_time - score_time < 1000:
+        num = game_font.render("3", False, light_grey)
+        screen.blit(num, (screen_width / 2 - 13, screen_height / 2 + 30))
+    if 1000 < current_time - score_time < 2000:
+        num = game_font.render("2", False, light_grey)
+        screen.blit(num, (screen_width / 2 - 13, screen_height / 2 + 30))
+    if 2000 < current_time - score_time < 3000:
+        num = game_font.render("1", False, light_grey)
+        screen.blit(num, (screen_width / 2 - 13, screen_height / 2 + 30))
+
+    if current_time - score_time < 3000:
+        ball_speed_x, ball_speed_y = 0, 0
+    else:
+        ball_speed_x = 5 * random.choice((1, -1))
+        ball_speed_y = 5 * random.choice((1, -1))
+        score_time = 0
+
+
 
 
 # General Setup
@@ -71,6 +90,7 @@ ball_speed_x = 5 * random.choice((1, -1))
 ball_speed_y = 5 * random.choice((1, -1))
 player_speed = 0
 opponent_speed = 7
+score_time = 1
 
 # Text Variables
 player_score = 0
@@ -107,10 +127,13 @@ while run:
     pygame.draw.ellipse(screen, light_grey, ball)
     pygame.draw.aaline(screen, light_grey, (screen_width / 2, 0), (screen_width / 2, screen_height))
 
+    if score_time != 0:
+        ball_restart()
+
     player_text = game_font.render(f"{player_score}", False, light_grey)
-    screen.blit(player_text, (screen_width / 2 - 13, screen_height / 2))
+    screen.blit(player_text, (screen_width / 2 - 13, screen_height / 2 + 10))
     opponent_text = game_font.render(f"{opponent_score}", False, light_grey)
-    screen.blit(opponent_text, (screen_width / 2 + 5, screen_height / 2))
+    screen.blit(opponent_text, (screen_width / 2 + 5, screen_height / 2 + 10))
 
     pygame.display.flip()
     clock.tick(60)
